c074fbbfe4e133ed633793f5c72e0a8e6b5340ab
Skun01 <truongg9655@gmail.com>
2024-05-02 11:48:36 +0700

    starting coding js

---
+++ main.js
@@ -0,0 +1,13 @@
+
+const screenElem = document.querySelector(".screen");
+
+const resetElem = document.querySelector("#reset");
+const setupElem = document.querySelector("#setup");
+
+const pixel = 16;
+
+function makeScreen(screenElem, pixel){
+    for(let i = 0; i < pixel; i++){
+        const cell = document.createElement("div");
+    }
+}
